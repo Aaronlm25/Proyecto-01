fe7ed0531128eab27d531cadb8e959fc2b439c6b
Gibran-RL <gibranramirez@ciencias.unam.mx>
2024-08-26 19:14:08 -0600

    La clase weather ya implementa el metodo anterior para determinar el icono segun el id del clima :tada:

--- weather-app/weather.py
+++ weather-app/weather.py
@@ -12,7 +12,7 @@ class WeatherData:
         self.weather_id = self.json_data['weather'][0]['id']
         self.main_weather = self.json_data['weather'][0]['main']
         self.description = self.json_data['weather'][0]['description']
-        self.icon = self.json_data['weather'][0]['icon']
+        self.icon = self._determine_icon()
         
         # Información principal
         self.temp = self.json_data['main']['temp']
@@ -48,6 +48,7 @@ class WeatherData:
         return (f"Weather in {self.name} ({self.country}):\n"
                 f"Coordinates: lat {self.lat}, lon {self.lon}\n"
                 f"Weather ID: {self.weather_id}, Main: {self.main_weather}, Description: {self.description}\n"
+                f"Weather icon: {self.icon}\n"
                 f"Temperature: {self.temp}°C (Feels like: {self.feels_like}°C)\n"
                 f"Min Temp: {self.temp_min}°C, Max Temp: {self.temp_max}°C\n"
                 f"Pressure: {self.pressure} hPa, Humidity: {self.humidity}%\n"
@@ -56,7 +57,35 @@ class WeatherData:
                 f"Cloudiness: {self.cloudiness}%\n"
                 f"Sunrise: {self.sunrise}, Sunset: {self.sunset}\n"
                 f"Timezone: {self.timezone}, City ID: {self.id}, Response Code: {self.cod}")
+        
+    def _determine_icon(self):
+        """
+        Determina el icono basado en el ID del clima.
+        """
+        if 200 <= self.weather_id <= 232:
+            return "storm_icon.svg"  # Tormenta
+        elif 300 <= self.weather_id <= 321:
+            return "light_rain_icon.svg"  # Lluvia ligera
+        elif 500 <= self.weather_id <= 504:
+            return "rain_icon.svg"  # Lluvia
+        elif self.weather_id == 511:
+            return "snow_icon.svg"  # Nieve
+        elif 520 <= self.weather_id <= 531:
+            return "rain_icon.svg"  # Lluvia
+        elif 600 <= self.weather_id <= 622:
+            return "snow_icon.svg"  # Nieve
+        elif 701 <= self.weather_id <= 781:
+            return "fog_icon.svg"  # Neblina
+        elif self.weather_id == 800:
+            return "clear_icon.svg"  # Despejado
+        elif self.weather_id == 801:
+            return "partly_cloudy_icon.svg"  # Algunas nubes
+        elif self.weather_id == 802:
+            return "clouds_icon.svg"  # Nubes
+        elif 803 <= self.weather_id <= 804:
+            return "cloudy_icon.svg"  # Nublado
 
+#Solo PARA DEPURAR (ELIMINAR PARA ENTREGA )
 # Ejemplo de uso:
 if __name__ == "__main__":
     # Simulación de un JSON obtenido de una API o archivo
