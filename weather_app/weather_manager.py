0c97c9806fcf1b06113d5dca594b232ad59f7267
Gibran-RL <gibranramirez@ciencias.unam.mx>
2024-09-02 22:19:22 -0600

    :zap: Refactorizaion de los tipos de busqueda en index se pasaron a weather_manager

--- weather_app/weather_manager.py
+++ weather_app/weather_manager.py
@@ -1,6 +1,8 @@
 import requests
 import csv
 import threading
+import static.python.gather as gatherer
+
 KEY = 'a3117bc0d7c113aba1f25b2fb28748e1'
 LOCK = threading.Lock()
 def get_weather(city):
@@ -45,3 +47,35 @@ def save_flight_data_to_txt(flight_data, txt_file_path):
                 file.write(f"{flight_number}: Departure: {info['departure']}, Arrival: {info['arrival']}\n")
     except IOError as e:
         print(f"Error al escribir el archivo {txt_file_path}: {e}")
+        
+def seach_by_iata(iata_code):
+    weather_data={}
+    city = gatherer.get_city(iata_code)
+    if city:
+        weather_data['city'] =get_weather(city)
+    else:
+        weather_data['error'] = "Código IATA no válido."
+    return weather_data
+        
+def seach_by_city(city):
+    weather_data={}
+    weather_data['city']=get_weather(city)
+    return weather_data
+        
+def seach_by_id(flight_number):
+    weather_data={}
+    flight_info = gatherer.buscar_vuelo(flight_number)
+    if isinstance(flight_info, dict):  # Asegúrate de que es un diccionario
+        departure = flight_info['departure']
+        arrival = flight_info['arrival']               
+    else:
+        weather_data['error']= flight_info  # Mensaje de error si no se encontró el vuelo
+            
+        #Busca si la llegada o la salida es la ubicacion distinta de mexico
+    if departure == "MEX":
+        city=gatherer.get_city(departure)
+    else:
+        city=gatherer.get_city(arrival)
+            
+    weather_data['city']=get_weather(city)
+    return weather_data
