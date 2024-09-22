222cffaef84f4d2359a48409153989f2cdd852cc
Luis <luiscarrillo@ciencias.unam.mx>
2024-09-22 17:57:20 -0600

    Creacion de los 3 templates, también se renderizan los 3

--- weather_app/index.py
+++ weather_app/index.py
@@ -50,7 +50,65 @@ def home():
             print('No se encontró el URL')
         except TypeError as e:
             error_message = str(e)
-    return render_template('index.html', departure_weather=departure_weather, arrival_weather=arrival_weather, error=error_message, datalist_options = datalist_options)
+    return render_template('home.html', departure_weather=departure_weather, arrival_weather=arrival_weather, error=error_message, datalist_options = datalist_options)
+
+@app.route('/city.html', methods=['GET', 'POST'])
+def city():
+    arrival_weather = None
+    error_message = None
+    datalist_options = DATA_MANAGER.get_cities()
+    if request.method == 'POST':
+        city = request.form.get('city')
+        iata_code = request.form.get('iata_code')
+        try:
+            if city:
+                city_weather = weather_manager.search_by_city(city, weather_cache.get_data())
+                departure_weather = city_weather
+            elif iata_code:
+                iata_weather = weather_manager.search_by_iata(iata_code, weather_cache.get_data())
+                departure_weather = iata_weather
+            if arrival_weather:
+                weather_cache.update(arrival_weather)
+        except ValueError as e:
+            error_message = str(e)
+        except RequestException as e:
+            error_message = str(e)
+        except AttributeError as e:
+            print(f"Error al actualizar la caché: {e}")
+        except HTTPError as e:
+            print('No se encontró el URL')
+        except TypeError as e:
+            error_message = str(e)
+    return render_template('city.html', arrival_weather=arrival_weather, error=error_message, datalist_options = datalist_options)
+
+@app.route('/flight.html', methods=['GET', 'POST'])
+def flight():
+    departure_weather = None
+    arrival_weather = None
+    error_message = None
+    datalist_options = DATA_MANAGER.get_cities()
+    if request.method == 'POST':
+        flight_number = request.form.get('flight_number')
+        try:
+            if flight_number:
+                flight_weather = weather_manager.search_by_id(flight_number, weather_cache.get_data())
+                departure_weather = flight_weather[0]
+                arrival_weather = flight_weather[1]
+            if departure_weather:
+                weather_cache.update(departure_weather)
+            if arrival_weather:
+                weather_cache.update(arrival_weather)
+        except ValueError as e:
+            error_message = str(e)
+        except RequestException as e:
+            error_message = str(e)
+        except AttributeError as e:
+            print(f"Error al actualizar la caché: {e}")
+        except HTTPError as e:
+            print('No se encontró el URL')
+        except TypeError as e:
+            error_message = str(e)
+    return render_template('flight.html', departure_weather=departure_weather, arrival_weather=arrival_weather, error=error_message)
 
 if __name__ == '__main__':
     safe_stop = lambda signal, frame: (weather_cache.stop(), sys.exit(0))
