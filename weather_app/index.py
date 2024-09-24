bb6389414b3edfd02babb061e0b4fb4fcc296b3f
Luis <luiscarrillo@ciencias.unam.mx>
2024-09-23 19:39:49 -0600

    Se agregaron direccionamientos entre los 3 templates dependiendo de la eleccion del usuario

--- weather_app/index.py
+++ weather_app/index.py
@@ -17,6 +17,7 @@ weather_cache = Cache('./weather_app/static/json/cache.json')
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
+    template = 'home.html'
     departure_weather = None
     arrival_weather = None
     error_message = None
@@ -25,7 +26,13 @@ def home():
         city = request.form.get('city')
         iata_code = request.form.get('iata_code')
         flight_number = request.form.get('flight_number')
+        option = request.form.get('option')
         try:
+            if option:
+                if option == 'flight_number':
+                    template = 'flight.html'
+                elif option in ['city', 'iata_code']:
+                    template = 'city.html'
             if flight_number:
                 flight_weather = weather_manager.search_by_id(flight_number, weather_cache.get_data())
                 departure_weather = flight_weather[0]
@@ -50,65 +57,7 @@ def home():
             print('No se encontró el URL')
         except TypeError as e:
             error_message = str(e)
-    return render_template('home.html', departure_weather=departure_weather, arrival_weather=arrival_weather, error=error_message, datalist_options = datalist_options)
-
-@app.route('/city.html', methods=['GET', 'POST'])
-def city():
-    arrival_weather = None
-    error_message = None
-    datalist_options = DATA_MANAGER.get_cities()
-    if request.method == 'POST':
-        city = request.form.get('city')
-        iata_code = request.form.get('iata_code')
-        try:
-            if city:
-                city_weather = weather_manager.search_by_city(city, weather_cache.get_data())
-                departure_weather = city_weather
-            elif iata_code:
-                iata_weather = weather_manager.search_by_iata(iata_code, weather_cache.get_data())
-                departure_weather = iata_weather
-            if arrival_weather:
-                weather_cache.update(arrival_weather)
-        except ValueError as e:
-            error_message = str(e)
-        except RequestException as e:
-            error_message = str(e)
-        except AttributeError as e:
-            print(f"Error al actualizar la caché: {e}")
-        except HTTPError as e:
-            print('No se encontró el URL')
-        except TypeError as e:
-            error_message = str(e)
-    return render_template('city.html', arrival_weather=arrival_weather, error=error_message, datalist_options = datalist_options)
-
-@app.route('/flight.html', methods=['GET', 'POST'])
-def flight():
-    departure_weather = None
-    arrival_weather = None
-    error_message = None
-    datalist_options = DATA_MANAGER.get_cities()
-    if request.method == 'POST':
-        flight_number = request.form.get('flight_number')
-        try:
-            if flight_number:
-                flight_weather = weather_manager.search_by_id(flight_number, weather_cache.get_data())
-                departure_weather = flight_weather[0]
-                arrival_weather = flight_weather[1]
-            if departure_weather:
-                weather_cache.update(departure_weather)
-            if arrival_weather:
-                weather_cache.update(arrival_weather)
-        except ValueError as e:
-            error_message = str(e)
-        except RequestException as e:
-            error_message = str(e)
-        except AttributeError as e:
-            print(f"Error al actualizar la caché: {e}")
-        except HTTPError as e:
-            print('No se encontró el URL')
-        except TypeError as e:
-            error_message = str(e)
-    return render_template('flight.html', departure_weather=departure_weather, arrival_weather=arrival_weather, error=error_message)
+    return render_template(template, departure_weather=departure_weather, arrival_weather=arrival_weather, error=error_message, datalist_options = datalist_options)
 
 if __name__ == '__main__':
     safe_stop = lambda signal, frame: (weather_cache.stop(), sys.exit(0))
