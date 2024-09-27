e476fd06cba5e3af374010baa1b409d7e7b99976
Luis <luiscarrillo@ciencias.unam.mx>
2024-09-26 22:07:36 -0600

    Se agregaron tarjetas para ver más detalles sobre el clima asi como un botón que las despliega

---
+++ weather_app/static/js/show_more.js
@@ -0,0 +1,9 @@
+document.getElementById('see_more_button').addEventListener('click', function() {
+    const content = document.getElementById('see_more');
+    content.style.display = content.style.display === 'none' ? 'block' : 'none';
+});
+
+document.getElementById('see_more_button2').addEventListener('click', function() {
+    const content = document.getElementById('see_more2');
+    content.style.display = content.style.display === 'none' ? 'block' : 'none';
+});
