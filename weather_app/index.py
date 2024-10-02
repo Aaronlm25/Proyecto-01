0793a3243e06510edcd6fae5ed1ef19874c2720f
Pablo <pablomorafo@ciencias.unam.mx>
2024-10-01 19:37:32 -0600

    Se arreglo bug que regresaba a home despues de presionar enter en cuadro de texto vacio

--- weather_app/index.py
+++ weather_app/index.py
@@ -3,10 +3,12 @@ import sys
 import signal
 from cache import Cache
 from flask import Flask, render_template, request
-from requests.exceptions import RequestException, HTTPError
-from weather_exceptions import CityNotFoundError, IATANotFoundError, FlightNotFoundError, WeatherRequestError
 from static.python.data_manager import DataManager
 from autocorrect import revise
+from weather_exceptions import (CityNotFoundError, 
+                                IATANotFoundError, 
+                                FlightNotFoundError, 
+                                WeatherRequestError)
 
 app = Flask(__name__)
 
@@ -21,30 +23,29 @@ def home():
     suggestion = ''
     suggestions = []
     if request.method == 'POST':
-        city = str(request.form.get('city', '')).strip()
-        iata_code = str(request.form.get('iata_code', '')).strip()
-        flight_number = str(request.form.get('flight_number', '')).strip()
-        option = request.form.get('option')
+        city = None
+        iata_code = None
+        flight_number = None
+        option = get_option()
         try:
-            if option == 'flight_number':
+            if option == 'SEARCH_BY_FLIGHT':
+                flight_number = str(request.form.get('flight_number', '')).strip()
                 template = 'flight.html'
-            elif option == 'city':
+                if flight_number:
+                    flight_weather = weather_manager.search_by_id(flight_number, weather_cache.get_data())
+                    departure_weather = flight_weather[0]
+                    arrival_weather = flight_weather[1]
+            elif option == 'SEARCH_BY_CITY':
+                city = str(request.form.get('city', '')).strip()
                 template = 'city.html'
-            elif option == 'iata_code':
+                if city:
+                    suggestions = revise(city, data_collector.get_cities())
+                    departure_weather = weather_manager.search_by_city(city, weather_cache.get_data())
+            elif option == 'SEARCH_BY_IATA':
+                iata_code = str(request.form.get('iata_code', '')).strip().upper()
                 template = 'iata.html'
-            if flight_number:
-                template = 'flight.html'
-                flight_weather = weather_manager.search_by_id(flight_number, weather_cache.get_data())
-                departure_weather = flight_weather[0]
-                arrival_weather = flight_weather[1]
-            elif city:
-                suggestions = revise(city, data_collector.get_cities())
-                template = 'city.html'
-                departure_weather = weather_manager.search_by_city(city, weather_cache.get_data())
-            elif iata_code:
-                iata_upper = iata_code.upper()
-                template = 'iata.html'
-                departure_weather = weather_manager.search_by_iata(iata_upper, weather_cache.get_data())
+                if iata_code:
+                    departure_weather = weather_manager.search_by_iata(iata_code, weather_cache.get_data())
             if departure_weather:
                 weather_cache.update(departure_weather)
             if arrival_weather:
@@ -69,6 +70,16 @@ def home():
         suggestion=suggestion,
         city=city
     )
+
+def get_option():
+    option = request.form.get('option')
+    if option == 'flight_number' or 'flight_number' in request.form:
+        return 'SEARCH_BY_FLIGHT'
+    elif option == 'city' or 'city' in request.form:
+        return 'SEARCH_BY_CITY'
+    elif option == 'iata_code' or 'iata_code' in request.form:
+        return 'SEARCH_BY_IATA'
+    return 'UNKNOWN_OPTION'
 
 @app.route('/correct', methods=['GET'])
 def correct():
